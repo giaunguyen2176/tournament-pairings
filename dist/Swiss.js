a9a39093f2c35262a2f387d93b34534a92cf0e21
Giau Nguyen <minhgiau.nguyen@gmail.com>
2024-09-15 22:58:23 +0700

    swiss split group

--- dist/Swiss.js
+++ dist/Swiss.js
@@ -69,7 +69,9 @@ export function Swiss(players, round, rated = false, colors = false) {
         console.debug("score, evenHighThreshold, evenLowThreshold, evenSlicePlayerCount", curr.score, evenHighThreshold, evenLowThreshold, evenSlicePlayerCount);
         const evenSlicePlayers = playerArray.filter((p) => p.score < evenHighThreshold && p.score >= evenLowThreshold);
         console.log("evenSlicePlayers", evenSlicePlayers);
-        const halfway = evenSlicePlayerCount / 2;
+        const sliceBeginIndex = playerArray.indexOf(evenSlicePlayers[0]);
+        const sliceEndIndex = playerArray.indexOf(evenSlicePlayers.at(-1));
+        const halfway = (sliceBeginIndex + sliceEndIndex + 1) / 2;
         for (let j = 0; j < next.length; j++) {
             const opp = next[j];
             if (curr.hasOwnProperty("avoid") && curr.avoid.includes(opp.id)) {
